fcd35e6de6d6f9ce8fda5618c87971a42a540598
parth krishnan <69578065+Parthkrishnan@users.noreply.github.com>
2020-10-01 17:44:20 +0530

    Update lenear_search.c

--- lenear_search.c
+++ lenear_search.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main()
 {
 int a[10],i,n,item,flag=0;
-printf("enter the no of elements");
+printf("Enter the nnumber of elements: ");
 scanf("%d",&n);
-printf("enter the no of array");
+printf("Enter the number of array: ");
 for(i=0;i<n;i++)
   {
-  scanf("%d",&a[i]);
+  scanf("%d ",&a[i]);
   }
 printf("enter the item\n");
 scanf("%d",&item);
@@ -15,13 +17,13 @@ for(i=0;i<n;i++)
 {
 if(a[i]==item)
 {
-printf("no is found");
+printf("Number has been found. ");
 flag=1;
 }
 }
 if(flag==0)
 {
-printf("number is not found in the array\n");
+printf("Number is not found in the array\n");
 }
 return 0;
 }
